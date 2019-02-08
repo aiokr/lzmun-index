b09f090a292affa4d0355200260fcde328ae0b52
aiokr <hiaioke@gmail.com>
2019-02-08 22:04:41 +0800

    添加Gitment

---
+++ themes/mdui/source/js/gitment.js
@@ -0,0 +1,12 @@
+const myTheme = {
+    render(state, instance) {
+        const container = document.createElement('div')
+        container.lang = "en-US"
+        container.className = 'gitment-container gitment-root-container'
+        container.appendChild(instance.renderHeader(state, instance))
+        container.appendChild(instance.renderEditor(state, instance))
+        container.appendChild(instance.renderComments(state, instance))
+        container.appendChild(instance.renderFooter(state, instance))
+        return container
+    },
+}
